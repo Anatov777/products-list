dfc17eaa3cd24073b1d866c3e720723213d009c0
Anatov777 <anatovtimur@gmail.com>
2020-10-21 13:20:24 +0300

    del comments

--- src/components/ProductList.vue
+++ src/components/ProductList.vue
@@ -1,13 +1,11 @@
 <template>
   <div id="products_section">
     <div class="products_page pg_0">
-      <!-- <div
-        class="product product_horizontal"
+      <Product
         v-for="(item, index) in products"
         :key="item.productId + '%' + index"
-      > -->
-        <Product v-for="(item, index) in products" :key="item.productId + '%' + index" :prodData="item"/>
-      <!-- </div> -->
+        :prodData="item"
+      />
     </div>
   </div>
 </template>
@@ -23,42 +21,12 @@ export default {
   data() {
     return {
       products: this.getProducts(),
-      // productNumber: '1',
-      // priceType: 'meter',
     };
   },
-  // watch: {
-  //   productNumber: function () {
-  //     if (this.productNumber < 1) {
-  //       this.productNumber = 1;
-  //     }
-  //   },
-  //   priceType: function () {
-  //     if (this.priceType === 'pack') {
-  //       console.log(event);
-  //     }
-  //   }
-  // },
   methods: {
     getProducts() {
       return require("@/products.json");
     },
-    // formatAssoc(elem, arr) {
-    //   if (elem === arr[arr.length - 2]) {
-    //     return elem + ".";
-    //   } else if (elem === "") {
-    //     return elem;
-    //   } else {
-    //     return elem + ",";
-    //   }
-    // },
-
-
-    // imgUrlModif(imgUrl) {
-    //   let tmpArr = imgUrl.split('.')
-    //   tmpArr.splice(-1, 0, '_220x220_1.');
-    //   return tmpArr.join('');
-    // },
   },
 };
 </script>
